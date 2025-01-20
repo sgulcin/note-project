7d168cd653e4d8c1e4418d2f4c32d5fdb7dd2304
sgulcin <sgulcingirgin@gmail.com>
2025-01-20 23:12:58 +0300

    note app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+      container: {
+        center: true,
+      },
+      extend: {
+        backgroundImage: {
+          'forest': "url('../src/assest/forest.jpg')",
+        },
+      },
+    },
+    plugins: [],
+  }
